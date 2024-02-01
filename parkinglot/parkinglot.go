da984dfa6de086a9d323928e549df532538bfd3f
SohamMDoshi <soham.doshi_ftc@external.swiggy.in>
2024-01-31 21:58:52 -0800

    Park method created and tested pass

--- parkinglot/parkinglot.go
+++ parkinglot/parkinglot.go
@@ -1,5 +1,11 @@
 package parkinglot
 
+import "errors"
+
+var (
+	ErrParkingLotfull = errors.New("Parking lot is full")
+)
+
 type ParkingLot struct {
 	ID    string
 	Slots []Slot
@@ -8,4 +14,14 @@ type ParkingLot struct {
 func NewParkingLot(id string, slotCount int) *ParkingLot {
 	slots := make([]Slot, slotCount)
 	return &ParkingLot{ID: id, Slots: slots}
+}
+
+func (p *ParkingLot) Park(car Car) (int, error) {
+	for i, slot := range p.Slots {
+		if slot.Status == Empty {
+			p.Slots[i] = Slot{Status: Full, Car: car}
+			return i, nil
+		}
+	}
+	return -1, ErrParkingLotfull
 }
--- parkinglot/parkinglot_test.go
+++ parkinglot/parkinglot_test.go
@@ -14,3 +14,26 @@ func TestNewParkingLot(t *testing.T) {
 	assert.Equal(t, id, parkingLot.ID)
 	assert.Len(t, parkingLot.Slots, slotCount)
 }
+
+func TestParkCar(t *testing.T) {
+	parkinglot := NewParkingLot("PL001", 1)
+
+	car := Car{Color: "Red", RegistrationNum: "MH07SS8888"}
+	slotNum, err := parkinglot.Park(car)
+	assert.NoError(t, err)
+	assert.Equal(t, Full, parkinglot.Slots[slotNum].Status)
+}
+
+func TestParkCarWhenParkingLotIsFull(t *testing.T) {
+	parkinglot := NewParkingLot("PL001", 1)
+
+	car1 := Car{Color: "Red", RegistrationNum: "MH07SS8888"}
+	slotNum, err := parkinglot.Park(car1)
+	assert.NoError(t, err)
+	assert.Equal(t, Full, parkinglot.Slots[slotNum].Status)
+
+	car2 := Car{Color: "Black", RegistrationNum: "JK09GG1012"}
+	_, err1 := parkinglot.Park(car2)
+	assert.EqualError(t, err1, ErrParkingLotfull.Error())
+
+}
